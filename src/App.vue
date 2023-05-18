7a5a49195297b7b8ee9ab170b3fedf1293a3d22c
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-05-18 22:25:35 +0800

    chore: support webview ctrl + c

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,32 @@ import { themeColor } from './store'
 const style = computed<any>(() => ({
   '--theme-color': themeColor.value,
 }))
+let text = ''
+
+onMounted(() => {
+  document.addEventListener('keydown', (e) => {
+    if ((e.metaKey || e.ctrlKey) && e.key === 'c')
+      window.parent.postMessage({ eventType: 'copy', text }, '*')
+  })
+  document.addEventListener('selectionchange', () => {
+    const selection = document.getSelection()!
+    if (!selection || !selection.toString())
+      return
+    text = selection.toString()
+  })
+})
+onUnmounted(() => {
+  document.removeEventListener('keydown', (e) => {
+    if ((e.metaKey || e.ctrlKey) && e.key === 'c')
+      window.parent.postMessage({ eventType: 'copy', text }, '*')
+  })
+  document.removeEventListener('selectionchange', () => {
+    const selection = document.getSelection()!
+    if (!selection || !selection.toString())
+      return
+    text = selection.toString()
+  })
+})
 </script>
 
 <template>
